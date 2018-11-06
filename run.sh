dbff61b194bcbb23174514640702462b3670d0df
Nikita Pokidyshev <nik96lip@gmail.com>
2018-11-06 17:08:32 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-nvidia-docker run -d --ipc=host -it -p 9999:9999 -v /home/npokidyshev/:/my -v /opt/:/opt -v /mnt:/mnt --name sandpiturtle_lab sandpiturtle/lab
+nvidia-docker run -dit --restart unless-stopped --ipc=host -p 9999:9999 -v /home/npokidyshev/:/my -v /opt/:/opt -v /mnt:/mnt --name sandpiturtle_lab sandpiturtle/lab
